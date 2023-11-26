47b4c4c0069418ae61602c1c4319cea6e50242ac
zagob <matheusbestana@gmail.com>
2023-11-26 20:29:30 -0300

    Navbar

---
+++ src/config/index.ts
@@ -0,0 +1,23 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits" as const,
+        featured: [
+            {
+                name: 'Editor picks',
+                href: '#',
+                imageSrc: '/nav/ui-kits/mixed.jpg'
+            },
+            {
+                name: 'New Arrivals',
+                href: '#',
+                imageSrc: '/nav/ui-kits/blue.jpg'
+            },
+            {
+                name: 'Bestsellers',
+                href: '#',
+                imageSrc: '/nav/ui-kits/purple.jpg'
+            },
+        ]
+    }
+]
